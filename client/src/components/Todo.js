953b3a57702fae6a8d6dc8fbcbd3af3632cefd48
lielgrott <lielgrott@gmail.com>
2022-08-15 21:52:30 +0300

    edding a little css + doing some funcions in todo

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
-import TodoForm from './TodoForm'
-import TodoList from './TodoList';
+import React, { useState } from 'react';
+import { RiCloseCircleLine } from 'react-icons/ri';
+import { TiEdit } from 'react-icons/ti';
 
-function Todo() {
+import TodoForm from './TodoForm'
+import TodoList from './TodoList'
+
+
+function Todo({ todos, completeTodo, removeTodo }) {
     const [edit, setEdit] = useState({
         id: null,
         value: ''
     });
     return todos.map((todo, index) => {
-        <div className={todo.iscomplete ? 'todo-row complete' : 'todo-row'} key={index}></div>
+        <div className={todo.iscomplete ? 'todo-row complete' : 'todo-row'} key={index}>
+            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+                {todo.text}
+            </div>
+            <div className='icons'>
+                <RiCloseCircleLine
+                    onClick={() => removeTodo(todo.id)}
+                    className='delete-icon' />
+                <TiEdit
+                    onClick={() => setEdit({ id: todo.id, value: todo.text })}
+                    className='edit-icon' />
+            </div>
+        </div>
     })
 }
 
