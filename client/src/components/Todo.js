064ad81381b59a5e1e6caa28d149c32035d81fdb
lielgrott <lielgrott@gmail.com>
2022-08-16 22:11:37 +0300

    adding data to component(not completed)

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
+import axios from 'axios';
+
+
 
 function Todo({ todos, completeTodo, removeTodo }) {
     const [edit, setEdit] = useState({
         id: null,
         value: ''
     });
+
+    const [todo, setTodo] = useState([]);
+    useEffect(() => {
+        axios.get("http://localhost/api/todo")
+            .then((res) => console.log(res.data))
+            .catch((err) => console.log(err))
+    });
+
     return todos.map((todo, index) => {
         <div className={todo.iscomplete ? 'todo-row-complete' : 'todo-row'} key={index}>
             <div key={todo.id} onClick={() => completeTodo(todo.id)}>
